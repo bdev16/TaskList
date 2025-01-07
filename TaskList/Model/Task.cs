28c03401e6efc6adc582aaa27f2719bc958d2af4
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-07 16:04:40 -0300

    feat(Model/Task): Inicializa os atributos strings como vazio

--- TaskList/Model/Task.cs
+++ TaskList/Model/Task.cs
@@ -1,12 +1,14 @@
-﻿namespace TaskList.Model
+﻿using System.Runtime.CompilerServices;
+
+namespace TaskList.Model
 {
     public class Task
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public DateTime Date { get; set; }
-        public string Status { get; set; }
+        public string Status { get; set; } = string.Empty;
         public User User { get; set; }
     }
 }
