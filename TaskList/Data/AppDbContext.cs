6e0c640188831ccfbc73f89c3376b934769300d8
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-07 16:22:01 -0300

    feat(Data/AppDbContext): Define a tabela Tasks no banco de dados baseada na classe Task

--- TaskList/Data/AppDbContext.cs
+++ TaskList/Data/AppDbContext.cs
@@ -7,5 +7,8 @@ namespace TaskList.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Task> Tasks { get; set; }
+        
     }
 }
