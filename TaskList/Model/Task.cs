1056ca5cd39bb5fce435d526585b4ed76c6c5024
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-07 14:50:02 -0300

    feat(Models/Task): Define um objeto de User como uma propiedade
    Esse commit tem o intuito de realizar o relacionamento da classe User
    com a classe Task. A classe User pode ter varias task atribudas a si e a
    classe Task pode ter um e somente um User atribuido a si

--- TaskList/Model/Task.cs
+++ TaskList/Model/Task.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; }
         public DateTime Date { get; set; }
         public string Status { get; set; }
+        public User User { get; set; }
     }
 }
