4e197b1e48fc5a28453788a1f1ab116c6925cdb2
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-08 15:24:07 -0300

    feat(Migrations): Cria a migration PopulationUsers
    Esse commit tem o intuito de adicionar valores dentro da tabela tasks
    dentro do banco de dados, populando essa tabela

---
+++ TaskList/Migrations/20250108180504_PopulationUsers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+#nullable disable
+
+namespace TaskList.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulationUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Users(Name, Email, Password) VALUES('Bruno', 'brunoantonio0210@gmail.com', 'Bruno123')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Users");
+        }
+    }
+}
