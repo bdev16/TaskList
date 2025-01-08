ef84da7b5e5578c8f01780470cd4ee17f17dda80
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-08 16:14:35 -0300

    feat(Model/User): Adiciona o construtor dentro da classe
    Esse commit tem o intuito de inicializar o atributo Tasks do tipo ICollection, buscando seguir o padrão de uso desse tipo

--- TaskList/Model/User.cs
+++ TaskList/Model/User.cs
@@ -1,7 +1,13 @@
-﻿namespace TaskList.Model
+﻿using System.Collections.ObjectModel;
+
+namespace TaskList.Model
 {
     public class User
     {
+        public User() 
+        { 
+            Tasks = new Collection<Task>();
+        }
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
