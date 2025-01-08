c8831beb0e46be1a72b2ee3cbacd235b6418ba54
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-07 22:10:50 -0300

    feat(Controllers): Cria o controlador de Task
    Esse commit tem o intuito de criar uma classe que vai ser responsavel
    por receber os valores enviados pelos usuarios e retornar as devidas
    respostas para os seus locais corretos. Esse commit também usa o
    mecanismo de injeção de dependencia, injetando um objeto da classe
    AppDbContext dentro do controlador.

---
+++ TaskList/Controllers/TaskController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskList.Data;
+
+namespace TaskList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TaskController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
