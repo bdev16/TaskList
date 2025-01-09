39812bdd6f2cce0b5e245ae3c561eabea669eb65
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-09 16:00:31 -0300

    feat(Controllers): Adiciona os DataAnnotations nos atributos das classes Task e User
    Esse commit tem o intuito de validar as informações dos atributos
    presente em ambas as classes

--- TaskList/Model/Task.cs
+++ TaskList/Model/Task.cs
@@ -1,13 +1,21 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace TaskList.Model
 {
     public class Task
     {
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "O Titulo não pode ser vazio...")]
+        [StringLength(100, ErrorMessage = "O titulo informado ultrapassa os limites de 100 caracteres...")]
         public string Title { get; set; } = string.Empty;
+        [StringLength(300, ErrorMessage = "O texto informado ultrapassa os limites de 300 caracteres...")]
         public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A data não pode ser vazia...")]
         public DateTime Date { get; set; }
+        [Required(ErrorMessage = "O Status deve ser definido...")]
+        [DataType(DataType.DateTime, ErrorMessage = "A Data informada não segue o formato 00/00/0000 00:00:00")]
         public string Status { get; set; } = string.Empty;
         public int UserId { get; set; }
         public User User { get; set; }
--- TaskList/Model/User.cs
+++ TaskList/Model/User.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TaskList.Model
 {
@@ -8,9 +9,14 @@ namespace TaskList.Model
         { 
             Tasks = new Collection<Task>();
         }
+        [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "O Nome não pode ser vazio...")]
+        [StringLength(80, ErrorMessage = "O Nome informado ultrapassa os limites de 80 caracteres...")]
         public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "O Email não pode ser vazio...")]
         public string Email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A senha não pode ser vazia...")]
         public string Password { get; set; } = string.Empty;
         public ICollection<Task> Tasks { get; set; }
     }
