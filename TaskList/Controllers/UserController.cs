fe04826f339e3f0c0431a0a64e4daf6f348ec476
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-13 14:41:47 -0300

    feat(Controllers/UserController): Adicionando o mecanismo de injeção de dependencia

--- TaskList/Controllers/UserController.cs
+++ TaskList/Controllers/UserController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TaskList.Data;
 
 namespace TaskList.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class UserController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
     }
 }
