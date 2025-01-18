292125e91144ca7b3c8a4289f25e8a3a87153e96
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-18 19:29:29 -0300

    feat(Program.cs): Adiciona ao container de serviços, instancias dos repositorios

--- TaskList/Program.cs
+++ TaskList/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using TaskList.Data;
+using TaskList.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
