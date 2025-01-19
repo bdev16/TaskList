7a21bada33f2dadd9e9e4ac1669f74d4bb678551
DevBruno <brunoantonio.dev0016@gmail.com>
2025-01-19 17:34:40 -0300

    refactor: acionando o swagger independente do ambiente

--- TaskList/Program.cs
+++ TaskList/Program.cs
@@ -26,12 +26,17 @@ builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
