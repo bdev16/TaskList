273e9f29bd8cc5b08e0a6195274333d51255a9bb
DevBruno <brunoantonio.dev0016@gmail.com>
2025-03-10 17:48:32 -0300

    feat: Joga as mudanças feitas no codigo para o banco de dados

--- TaskList/Migrations/20250306234745_AddDateAtributeOnTaskEntity.Designer.cs
+++ TaskList/Migrations/20250306234745_AddDateAtributeOnTaskEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaskList.Data;
@@ -11,9 +12,11 @@ using TaskList.Data;
 namespace TaskList.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250306234745_AddDateAtributeOnTaskEntity")]
+    partial class AddDateAtributeOnTaskEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -162,9 +165,8 @@ namespace TaskList.Migrations
 
                     NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
 
-                    b.Property<string>("Date")
-                        .IsRequired()
-                        .HasColumnType("text");
+                    b.Property<DateTime>("Date")
+                        .HasColumnType("timestamp with time zone");
 
                     b.Property<string>("Description")
                         .IsRequired()
