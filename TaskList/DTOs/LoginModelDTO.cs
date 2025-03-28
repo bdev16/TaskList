2f1239c7f7c16f1891d16230e81bf107dc9ca66e
DevBruno <brunoantonio.dev0016@gmail.com>
2025-03-28 08:32:27 -0300

    Refactor(DTOs/LoginModelDTO): Muda o atributo Passoword para Email

--- TaskList/DTOs/LoginModelDTO.cs
+++ TaskList/DTOs/LoginModelDTO.cs
@@ -6,7 +6,7 @@ namespace TaskList.DTOs
     public class LoginModelDTO
     {
         [Required(ErrorMessage = "User name is required")]
-        public string? UserName { get; set; }
+        public string? Email { get; set; }
 
 		[Required(ErrorMessage = "Password is required")]
 		public string? Password { get; set; }
