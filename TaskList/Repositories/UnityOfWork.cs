3491e10b915583351a508960366935ba4e9c3712
DevBruno <brunoantonio.dev0016@gmail.com>
2025-04-14 20:58:28 -0300

    feat(Repositories): Cria a classe concreta UnityOfWork
    Esse commit tem o intuito de dar inicio a implementação do padrão
    UnityOfWork dentro do programa.
    
    Cria a classe UnityOfWork que vai implementar os métodos e propiedades da interface
    IUnityOfWork. A UnityOfWork vai conter instancias dos repositories
    dentro de si e métodos como o Commit e o Dispose que vão ser
    responsaveis por Salvar as mudanças no banco de dados e remover algumas
    coisas que não estão em uso no contexto do banco de dados
    respectivamente.

---
+++ TaskList/Repositories/UnityOfWork.cs
@@ -0,0 +1,47 @@
+﻿using TaskList.Data;
+
+namespace TaskList.Repositories
+{
+    public class UnityOfWork : IUnityOfWork
+    {
+        private ITaskRepository _taskRepository;
+        private IUserRepository _userRepository;
+        public AppDbContext _context;
+
+        public UnityOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public ITaskRepository TaskRepository
+        {
+            get
+            {
+                if (_taskRepository == null)
+                {
+                    _taskRepository = new TaskRepository(_context);
+                }
+                return _taskRepository;
+            }
+        }
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                if (_userRepository == null)
+                {
+                    _userRepository = new UserRepository(_context);
+                }
+                return _userRepository;
+            }
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
