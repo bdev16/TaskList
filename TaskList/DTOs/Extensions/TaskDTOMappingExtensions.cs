207653e93649827b1cf62d6049bd52bef6c9af8b
DevBruno <brunoantonio.dev0016@gmail.com>
2025-04-19 16:43:53 -0300

    feat(DTOs/Extensions): Cria um método de extensão para as formas de mapeamento utilizadas no taskController
    Esse commit tem o intuito de remover codigos duplicados de mapeamento de
    entidade e dtos dentro do controller de task. A classe de extensão vai
    conter métodos que se repetem dentro do controlador de task, deixando o
    codigo mais limpo e com menos linhas

---
+++ TaskList/DTOs/Extensions/TaskDTOMappingExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Task = TaskList.Model.Task;
+
+namespace TaskList.DTOs.Extensions
+{
+    public static class TaskDTOMappingExtensions
+    {
+        public static TaskDTO ToTaskDTO(this Task task)
+        {
+            TaskDTO taskDTO = new TaskDTO()
+            {
+                Id = task.Id,
+                Title = task.Title,
+                Description = task.Description,
+                Date = task.Date,
+                Status = task.Status,
+                UserId = task.UserId,
+            };
+
+            return taskDTO;
+        }
+
+        public static Task ToTask(this TaskDTO taskDTO)
+        {
+            Task task = new Task()
+            {
+                Id = taskDTO.Id,
+                Title = taskDTO.Title,
+                Description = taskDTO.Description,
+                Date = taskDTO.Date,
+                Status = taskDTO.Status,
+                UserId = taskDTO.UserId,
+            };
+
+            return task;
+        }
+
+        public static IEnumerable<TaskDTO> ToTaskDTOList(this IEnumerable<Task> tasks)
+        {
+            var tasksDTO = new List<TaskDTO>();
+            foreach (var task in tasks)
+            {
+                TaskDTO taskDTO = new TaskDTO()
+                {
+                    Id = task.Id,
+                    Title = task.Title,
+                    Description = task.Description,
+                    Date = task.Date,
+                    Status = task.Status,
+                    UserId = task.UserId,
+                };
+                tasksDTO.Add(taskDTO);
+            }
+
+            return tasksDTO;
+        }
+    }
+}
