e814bec203800794e6b27718c233eab0adbb1f44
DevBruno <brunoantonio.dev0016@gmail.com>
2025-04-21 17:27:29 -0300

    feat(Extensions/DTOMappingProfile.cs): Cria a classe DTOMappingProfile
    Esse commit vai criar a classe DTOMappingProfile que vai ser uma classe
    que vai herdar de AutoMapper.Profile que vai ser a classe do AutoMapper
    que vai ser responsável por conter os métodos que vão ser utilizados

---
+++ TaskList/DTOs/Extensions/DTOMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+
+namespace TaskList.DTOs.Extensions
+{
+    public class DTOMappingProfile : Profile
+    {
+        public DTOMappingProfile() 
+        {
+            CreateMap<Task, TaskDTO>().ReverseMap();
+        }
+    }
+}
