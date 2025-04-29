518230c936dc7d3b5ceaa3040a271d6f34e7ed0d
DevBruno <brunoantonio.dev0016@gmail.com>
2025-04-29 16:25:12 -0300

    feat(DTOs): Cria um dto padrão para o usuario, chamado de UserDTO

---
+++ TaskList/DTOs/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using Task = TaskList.Model.Task;
+
+namespace TaskList.DTOs
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string userName { get; set; }
+        public string email { get; set; }
+        public ICollection<Task>? tasks { get; set; }
+    }
+}
