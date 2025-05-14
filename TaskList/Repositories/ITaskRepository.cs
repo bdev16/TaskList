f5f0612d88834118a50ba2864577fb52900feec1
DevBruno <brunoantonio.dev0016@gmail.com>
2025-05-14 19:48:35 -0300

    feat(Repositories/ITaskRepository): Define o método GetTasks
    Esse commit tem o intuito de definir o método base de recuperar todas as
    tasks, utilizando a tecnica de paginação de dados.

--- TaskList/Repositories/ITaskRepository.cs
+++ TaskList/Repositories/ITaskRepository.cs
@@ -1,10 +1,12 @@
 ﻿using TaskList.Model;
+using TaskList.Pagination;
 using Task = TaskList.Model.Task;
 
 namespace TaskList.Repositories
 {
     public interface ITaskRepository : IRepository<Task>  
     {
+        IEnumerable<Task> GetTasks(TasksParameters tasksParameters);
         IEnumerable<Task> GetTasksForDate(string id, string date);
     }
 }
