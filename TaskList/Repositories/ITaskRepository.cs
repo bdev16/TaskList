97f44c987a7f1719b6b686a11623e47c8a49c516
DevBruno <brunoantonio.dev0016@gmail.com>
2025-05-21 21:41:48 -0300

    feat(Repositories): Define os métodos GetTasksTitleFilter e GetTasksStatusFilter
    Esse commit tem o intuito de definir dois métodos responsaveis por
    conter a lógica de filtragem de dados com base em caracteristicas
    especificas de um classe especifica

--- TaskList/Repositories/ITaskRepository.cs
+++ TaskList/Repositories/ITaskRepository.cs
@@ -7,6 +7,8 @@ namespace TaskList.Repositories
     public interface ITaskRepository : IRepository<Task>  
     {
         PagedList<Task> GetTasks(TasksParameters tasksParameters);
+        PagedList<Task> GetTasksTitleFilter(TasksTitleFilter tasksTitleFilter);
+        PagedList<Task> GetTasksStatusFilter(TasksStatusFilter tasksStatusFilter);
         IEnumerable<Task> GetTasksForDate(string id, string date);
     }
 }
