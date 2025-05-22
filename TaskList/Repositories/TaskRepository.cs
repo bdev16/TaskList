31061b1dd7e079152cf078438251dc007bcac2f5
DevBruno <brunoantonio.dev0016@gmail.com>
2025-05-21 21:44:45 -0300

    feat(Repositories): Implementa os métodos GetTasksTitleFilter e GetTasksStatusFilter
    Esse commit tem o intuito de implementar os dois métodos criados na
    interface ITaskRepository, sendo esses métodos que vão definir a lógica
    de filtragem de instancias de task com o valor do title e status
    definidos pelo usuario na hora do request

--- TaskList/Repositories/TaskRepository.cs
+++ TaskList/Repositories/TaskRepository.cs
@@ -22,5 +22,33 @@ namespace TaskList.Repositories
         {
             return _context.Tasks.AsNoTracking().Where(task => task.UserId == id && task.Date == date).ToList();
         }
+
+        public PagedList<Task> GetTasksStatusFilter(TasksStatusFilter tasksStatusFilter)
+        {
+            var tasks = GetAll().AsQueryable();
+
+            if (!string.IsNullOrEmpty(tasksStatusFilter.Status))
+            {
+                tasks = tasks.Where(task => task.Status.ToString().Contains(tasksStatusFilter.Status));
+            }
+
+            var filteredTasks = PagedList<Task>.ToPagedList(tasks, tasksStatusFilter.PageNumber, tasksStatusFilter.PageSize);
+
+            return filteredTasks;
+        }
+
+        public PagedList<Task> GetTasksTitleFilter(TasksTitleFilter tasksTitleFilter)
+        {
+            var tasks = GetAll().AsQueryable();
+            
+            if (!string.IsNullOrEmpty(tasksTitleFilter.Title))
+            {
+                tasks = tasks.Where(task => task.Title.Contains(tasksTitleFilter.Title));
+            }
+
+            var filteredTasks = PagedList<Task>.ToPagedList(tasks, tasksTitleFilter.PageNumber, tasksTitleFilter.PageSize);
+
+            return filteredTasks;
+        }
     }
 }
