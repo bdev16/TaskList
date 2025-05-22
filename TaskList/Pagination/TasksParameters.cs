0c3328b043fc62c5f59da687c1ee9ebe9570108a
DevBruno <brunoantonio.dev0016@gmail.com>
2025-05-21 21:35:09 -0300

    refactor(Pagination/TasksParameters): Remove o código presente na classe e joga para a classe QueryStringParametes

--- TaskList/Pagination/TasksParameters.cs
+++ TaskList/Pagination/TasksParameters.cs
@@ -1,20 +1,7 @@
 ﻿namespace TaskList.Pagination
 {
-    public class TasksParameters
+    public class TasksParameters : QueryStringParameters
     {
-        const int maxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
-        private int _pageSize;
-        public int PageSize
-        {
-            get 
-            { 
-                return _pageSize; 
-            }
-            set
-            {
-                _pageSize = (value > maxPageSize)? maxPageSize : value;
-            }
-        }
+        
     }
 }
